d7eac11466e9756b455d5acd4e505e708ae841a8
suffering-git <72675268+suffering-git@users.noreply.github.com>
2025-11-26 21:50:04 -0800

    removed vs code extension installation lines

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # Install claude code
 # echo "Installing Claude CLI..."
 # npm install -g @anthropic-ai/claude-code
@@ -7,15 +8,5 @@
 echo "Installing Gemini CLI..."
 npm install -g @google/gemini-cli
 
-# Install vs code extensions
-if command -v code > /dev/null; then
-    echo "Installing VS Code extensions..."
-    code --install-extension google.gemini-cli-vscode-ide-companion
-    # code --install-extension anthropic.claude-code
-    code --install-extension github.remotehub
-    echo "VS Code extensions installation complete."
-else
-    echo "VS Code 'code' command not found. Skipping extension installation."
-fi
 
 
